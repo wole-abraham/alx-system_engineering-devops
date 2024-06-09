4a46a92021ffa35d76b67dd9c86d7d439b913699
wole-abraham <123215687+wole-abraham@users.noreply.github.com>
2024-06-09 23:52:00 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-#create a file school with the content i love school
-
-file {'/tmp/school':
-        ensure  => file,
-        owner   => 'www-data',
-        group   => 'www-data',
-        mode    => '0744',
-        content => 'I love Puppet',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# executes a command to terminate a process
-
-exec {'kill_killmenow_process':
-        command => '/usr/bin/pkill killmenow',
-        }
