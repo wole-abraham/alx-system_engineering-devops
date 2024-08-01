d64b53707479a27928eb13edfb2743dd81c1f570
abrahm <wole1702@gmail.com>
2024-08-01 03:30:15 +0100

    code

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,13 +8,12 @@ if __name__ == '__main__':
     employee_id = argv[1]
     base_url = 'https://jsonplaceholder.typicode.com/users'
     url = base_url + "/" + employee_id
-    
+
     employee = requests.get(url)
     username = employee.json().get('username')
 
     todo_url = url + '/todos'
     tasks = requests.get(todo_url).json()
-
 
     with open(f"{argv[1]}.csv", mode='w', encoding='utf-8') as file:
         for task in tasks:
