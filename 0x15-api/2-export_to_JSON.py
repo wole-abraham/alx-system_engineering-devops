888a321e8936c9e007cb8259001e5c1ab3211655
abrahm <wole1702@gmail.com>
2024-08-01 03:49:46 +0100

    code

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Python script uses an api https://jsonplaceholder.typicode.com"""
+
+import requests
+from sys import argv
+import json
+
+if __name__ == '__main__':
+    employee_id = argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com/users'
+    url = base_url + "/" + employee_id
+
+    employee = requests.get(url)
+    username = employee.json().get('username')
+
+    todo_url = url + '/todos'
+    tasks = requests.get(todo_url).json()
+
+    with open(f"{argv[1]}.json", mode='w', encoding='utf-8') as file:
+
+        info = [{"task": task.get('title'), "completed": task.get('completed'),
+                 "username": username} for task in tasks]
+        info = {employee_id: info}
+        file.write(json.dumps(info))
