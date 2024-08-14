d365e73c045c0fa6896ac25cfe860fa11dcfe97a
abrahm <wole1702@gmail.com>
2024-08-14 10:52:55 +0100

    code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
 """
-    python script returns the number of subscribers
-    for a given subreddit
+number of subscribers for a given subreddit
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-
     """
-    Queries the Reddit API to return the number of
-    subscribers for a given subreddit.
-    If an invalid subreddit is given, returns 0.
-
-    :param subreddit: The name of the subreddit.
-    :return: The number of subscribers or 0 if the subreddit is invalid.
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
 
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    headers = {'User-Agent': "python:subreddit.subscriber.counter:v1.0\
-(by /u/Responsible-Bonus-58)"}
-    base_url = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
-                            allow_redirects=False, headers=headers)
-    if base_url.status_code == 200:
-        subs = base_url.json().get('data').get('subscribers')
-        return subs
-    else:
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
---
+++ 0x16-api_advanced/tried.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    python script returns the number of subscribers
+    for a given subreddit
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+
+    """
+    Queries the Reddit API to return the number of
+    subscribers for a given subreddit.
+    If an invalid subreddit is given, returns 0.
+
+    :param subreddit: The name of the subreddit.
+    :return: The number of subscribers or 0 if the subreddit is invalid.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    headers = {'User-Agent': "python:subreddit.subscriber.counter:v1.0\
+(by /u/Responsible-Bonus-58)"}
+    base_url = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
+                            allow_redirects=False, headers=headers)
+    if base_url.status_code == 200:
+        subs = base_url.json().get('data').get('subscribers')
+        return subs
+    else:
+        return 0
