33abf2349555abf727eeaa47bfa66ead649544a5
abrahm <wole1702@gmail.com>
2024-08-14 01:31:46 +0100

    code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,8 +18,10 @@ def number_of_subscribers(subreddit):
     :param subreddit: The name of the subreddit.
     :return: The number of subscribers or 0 if the subreddit is invalid.
     """
+    headers = {'User-Agent': "python:subreddit.subscriber.counter:v1.0\
+(by /u/Responsible-Bonus-58)"}
     base_url = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
-                            allow_redirects=False)
+                            allow_redirects=False, headers=headers)
     if base_url.status_code == 200:
         subs = base_url.json()['data']['subscribers']
         return subs
